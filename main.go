abe978145a13fff6dd1c86c397a9fc672c57d7c2
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-07-21 10:32:23 +0600

    make code short

--- main.go
+++ main.go
@@ -8,6 +8,18 @@ import (
 	"strings"
 )
 
+// array of commands
+var commands = []string{
+	"git add .",
+	"git commit -m",
+	"git push origin gitpush",
+	"git checkout master",
+	"git pull origin main",
+	"git merge gitpush",
+	"git push origin main",
+	"git checkout gitpush",
+}
+
 func main() {
 	fmt.Print("Enter Commit Message: ")
 	reader := bufio.NewReader(os.Stdin)
@@ -15,33 +27,16 @@ func main() {
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	cmd1 := exec.Command("git", "add", ".")
-	cmd2 := exec.Command("git", "commit", "-m", strings.TrimSpace(cmdString))
-	cmd3 := exec.Command("git", "push", "origin", "dev_rafiq")
-	cmd4 := exec.Command("git", "checkout", "master")
-	cmd5 := exec.Command("git", "merge", "dev_rafiq")
-	cmd6 := exec.Command("git", "push", "origin", "master")
-	cmd7 := exec.Command("git", "checkout", "dev_rafiq")
-	cmd1.Stdout = os.Stdout
-	cmd1.Stderr = os.Stderr
-	cmd2.Stdout = os.Stdout
-	cmd2.Stderr = os.Stderr
-	cmd3.Stdout = os.Stdout
-	cmd3.Stderr = os.Stderr
-	cmd4.Stdout = os.Stdout
-	cmd4.Stderr = os.Stderr
-	cmd5.Stdout = os.Stdout
-	cmd5.Stderr = os.Stderr
-	cmd6.Stdout = os.Stdout
-	cmd6.Stderr = os.Stderr
-	cmd7.Stdout = os.Stdout
-	cmd7.Stderr = os.Stderr
-	cmd1.Run()
-	cmd2.Run()
-	cmd3.Run()
-	cmd4.Run()
-	cmd5.Run()
-	cmd6.Run()
-	cmd7.Run()
+	cmdString = strings.Replace(cmdString, "\n", "", -1)
+	commands[1] = commands[1] + " " + `"` + cmdString + `"`
+	for _, cmd := range commands {
+		fmt.Println(cmd)
+		command := exec.Command("bash", "-c", cmd)
+		command.Stdout = os.Stdout
+		command.Stderr = os.Stderr
+		if err := command.Run(); err != nil {
+			fmt.Fprintln(os.Stderr, err)
+		}
+	}
 
 }
