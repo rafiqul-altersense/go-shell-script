bf214114ef636fc1ad3b1c262eaa00f500e93014
rafiqul-altersense <rafiqul.altersense@gmail.com>
2023-07-21 10:12:24 +0600

    test

--- main.go
+++ main.go
@@ -9,19 +9,39 @@ import (
 )
 
 func main() {
-	fmt.Print("Enter command: ")
+	fmt.Print("Enter Commit Message: ")
 	reader := bufio.NewReader(os.Stdin)
 	cmdString, err := reader.ReadString('\n')
 	if err != nil {
 		fmt.Fprintln(os.Stderr, err)
 	}
-	cmdString = strings.TrimSuffix(cmdString, "\n")
-	cmd := exec.Command(cmdString) // exec.Command is for executing commands
-	cmd.Stderr = os.Stderr         // os.Stderr is for error output, this coms from os package
-	
-	cmd.Stdout = os.Stdout         // os.Stdout is for standard output , this comes from os package
-	var r_err = cmd.Run()                      // cmd.Run() is for running the command
-	if r_err != nil {
-		fmt.Fprintln(os.Stderr, r_err)
-	}
+	cmd1 := exec.Command("git", "add", ".")
+	cmd2 := exec.Command("git", "commit", "-m", strings.TrimSpace(cmdString))
+	cmd3 := exec.Command("git", "push", "origin", "dev_rafiq")
+	cmd4 := exec.Command("git", "checkout", "master")
+	cmd5 := exec.Command("git", "merge", "dev_rafiq")
+	cmd6 := exec.Command("git", "push", "origin", "master")
+	cmd7 := exec.Command("git", "checkout", "dev_rafiq")
+	cmd1.Stdout = os.Stdout
+	cmd1.Stderr = os.Stderr
+	cmd2.Stdout = os.Stdout
+	cmd2.Stderr = os.Stderr
+	cmd3.Stdout = os.Stdout
+	cmd3.Stderr = os.Stderr
+	cmd4.Stdout = os.Stdout
+	cmd4.Stderr = os.Stderr
+	cmd5.Stdout = os.Stdout
+	cmd5.Stderr = os.Stderr
+	cmd6.Stdout = os.Stdout
+	cmd6.Stderr = os.Stderr
+	cmd7.Stdout = os.Stdout
+	cmd7.Stderr = os.Stderr
+	cmd1.Run()
+	cmd2.Run()
+	cmd3.Run()
+	cmd4.Run()
+	cmd5.Run()
+	cmd6.Run()
+	cmd7.Run()
+
 }
